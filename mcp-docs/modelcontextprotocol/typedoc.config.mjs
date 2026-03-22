8708fa0781b50b1184c0cb2822d18e1bb71e8e4f
songdogwang <songdogwang@tencent.com>
2026-03-22 10:15:20 +0800

    Add mcp-docs

---
+++ mcp-docs/modelcontextprotocol/typedoc.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+/** @type {Partial<import("typedoc").TypeDocOptions>} */
+const config = {
+  out: "tmp",
+  sort: ["source-order"],
+  excludeInternal: true,
+  excludeTags: [
+    "@format",
+    "@maximum",
+    "@minimum",
+    "@TJS-type",
+    "@maxItems",
+  ],
+  jsDocCompatibility: {
+    exampleTag: false,
+  },
+  disableSources: true,
+  logLevel: "Error",
+  plugin: ["./typedoc.plugin.mjs"],
+};
+
+export default config;
